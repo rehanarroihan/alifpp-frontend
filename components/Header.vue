5fa414e58fc5c42b9a5d3b8fc5a5cfc55fb90a06
rehanarroihan <third2014project@gmail.com>
2020-09-02 04:25:42 +0700

    project config & templating stage 1

---
+++ components/Header.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div id="topbar" class="d-none d-lg-flex align-items-center fixed-top">
+      <div class="container d-flex">
+        <div class="contact-info mr-auto">
+          <i class="icofont-envelope"></i> <a href="mailto:support@alifpp.com">support@alifpp.com</a>
+          <i class="icofont-phone"></i> +62 816562807
+          <i class="icofont-fax"></i> +62 3185583611
+        </div>
+        <div class="social-links">
+          <a href="#" class="twitter"><i class="icofont-twitter"></i></a>
+          <a href="#" class="facebook"><i class="icofont-facebook"></i></a>
+          <a href="#" class="instagram"><i class="icofont-instagram"></i></a>
+          <a href="#" class="skype"><i class="icofont-skype"></i></a>
+          <a href="#" class="linkedin"><i class="icofont-linkedin"></i></i></a>
+        </div>
+      </div>
+    </div>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top">
+      <div class="container d-flex align-items-center">
+
+        <img
+          src="assets/img/alifpplogo.png"
+          class="logo mr-3"
+          style="height: 50px; padding: 0; margin: 0; border-radius: 50%;box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);"
+        />
+        <h1 class="logo mr-auto"><a href="index.html">Alifpp<span>.</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt=""></a>-->
+
+        <nav class="nav-menu d-none d-lg-block">
+          <ul>
+            <li class="active"><a href="index.html">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#team">Team</a></li>
+            <li class="drop-down"><a href="">Drop Down</a>
+              <ul>
+                <li><a href="#">Drop Down 1</a></li>
+                <li class="drop-down"><a href="#">Deep Drop Down</a>
+                  <ul>
+                    <li><a href="#">Deep Drop Down 1</a></li>
+                    <li><a href="#">Deep Drop Down 2</a></li>
+                    <li><a href="#">Deep Drop Down 3</a></li>
+                    <li><a href="#">Deep Drop Down 4</a></li>
+                    <li><a href="#">Deep Drop Down 5</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Drop Down 2</a></li>
+                <li><a href="#">Drop Down 3</a></li>
+                <li><a href="#">Drop Down 4</a></li>
+              </ul>
+            </li>
+            <li><a href="#contact">Contact</a></li>
+
+          </ul>
+        </nav><!-- .nav-menu -->
+
+      </div>
+    </header><!-- End Header -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style>
+
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,91 @@
+
+export default {
+  /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
+  mode: 'universal',
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'static',
+  /*
+  ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: 'assets/img/alifpp.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i' },
+      { rel: 'stylesheet', href: 'assets/vendor/bootstrap/css/bootstrap.min.css' },
+      { rel: 'stylesheet', href: 'assets/vendor/icofont/icofont.min.css' },
+      { rel: 'stylesheet', href: 'assets/vendor/boxicons/css/boxicons.min.css' },
+      { rel: 'stylesheet', href: 'assets/vendor/owl.carousel/assets/owl.carousel.min.css' },
+      { rel: 'stylesheet', href: 'assets/vendor/venobox/venobox.css' },
+      { rel: 'stylesheet', href: 'assets/vendor/aos/aos.css' },
+      { rel: 'stylesheet', href: 'assets/css/style.css' },
+    ],
+    script: [
+      { src: 'assets/vendor/jquery/jquery.min.js' },
+      { src: 'assets/vendor/bootstrap/js/bootstrap.bundle.min.js' },
+      { src: 'assets/vendor/jquery.easing/jquery.easing.min.js' },
+      { src: 'assets/vendor/php-email-form/validate.js' },
+      { src: 'assets/vendor/waypoints/jquery.waypoints.min.js' },
+      { src: 'assets/vendor/counterup/counterup.min.js' },
+      { src: 'assets/vendor/owl.carousel/owl.carousel.min.js' },
+      { src: 'assets/vendor/isotope-layout/isotope.pkgd.min.js' },
+      { src: 'assets/vendor/venobox/venobox.min.js' },
+      { src: 'assets/vendor/aos/aos.js' },
+      { src: 'assets/js/main.js' },
+    ],
+  },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  ** https://nuxtjs.org/guide/plugins
+  */
+  plugins: [
+  ],
+  /*
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
+  */
+  components: true,
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module'
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa'
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {},
+  /*
+  ** Build configuration
+  ** See https://nuxtjs.org/api/configuration-build/
+  */
+  build: {
+  }
+}
